e47d1f5fba88f4767e5249e2266fe14e5dfbeced
Nick Mai <nick.mai@outlook.com>
2019-07-14 15:23:05 -0700

    refactor to entities

--- src/game.ts
+++ src/game.ts
@@ -2,86 +2,35 @@ import * as CANNON from 'cannon'
 import * as THREE from 'three'
 import { Controls } from './controls'
 import { PointerLockScreen } from './pointer-lock-screen'
+import { PhysicsEngine } from './physics-engine'
+import { Entity } from './entity'
+import { PlayerEntity } from './player-entity'
+import { BoxEntity } from './box-entity';
 
-const PLAYER_MASS = 5, PLAYER_RADIUS = 1.3, DT = 1 / 60
+const DT = 1 / 60
 
 export class Game {
-
-  private world: CANNON.World = new CANNON.World()
-  private playerBody: CANNON.Body = new CANNON.Body({ mass: PLAYER_MASS })
-  private boxes: Array<CANNON.Body> = []
-  // private boxMeshes: Array<THREE.Mesh> = []
-  private balls: Array<CANNON.Body> = []
-  // private ballMeshes: Array<THREE.Mesh> = []
   private camera: THREE.Camera
   private controls: Controls
   private time: number
   private renderer: THREE.Renderer
   private scene: THREE.Scene
-
-
-  private meshes: Array<THREE.Mesh> = []
-  
+  private phys: PhysicsEngine
+  private entities: Array<Entity>
+  private playerEntity: PlayerEntity
 
   constructor() {
-    this.initWorld()
-    this.initCannon()
-    this.initScene()
-    this.animate()
-  }
-
-  private initWorld() {
-    // Setup our world
-    this.world.quatNormalizeSkip = 0
-    this.world.quatNormalizeFast = false
-
-    let solver = new CANNON.GSSolver()
-
-    this.world.defaultContactMaterial.contactEquationStiffness = 1e9
-    this.world.defaultContactMaterial.contactEquationRelaxation = 4
-
-    solver.iterations = 7
-    solver.tolerance = 0.1
-    var split = true
-    if (split)
-      this.world.solver = new CANNON.SplitSolver(solver)
-    else
-      this.world.solver = solver
-
-    this.world.gravity.set(0, -40, 0)
-    this.world.broadphase = new CANNON.NaiveBroadphase()
-  }
-
-  private initCannon() {
-    let physicsMaterial, walls = []
+    this.phys = new PhysicsEngine()
+    this.entities = []
     
-    let playerShape = new CANNON.Sphere(PLAYER_RADIUS)
+    this.initScene()
 
-    // Create a slippery material (friction coefficient = 0.0)
-    physicsMaterial = new CANNON.Material("slipperyMaterial")
-    var physicsContactMaterial = new CANNON.ContactMaterial(
-      physicsMaterial,
-      physicsMaterial,
-      {
-        friction: 0.0,
-        restitution: 0.3
-      }
-    )
-    // We must add the contact materials to the world
-    this.world.addContactMaterial(physicsContactMaterial)
+    this.playerEntity = new PlayerEntity(this.phys.world, this.scene)
+    this.entities.push(this.playerEntity)
 
-    // Create a sphere
-    this.playerBody.addShape(playerShape)
-    this.playerBody.position.set(0, 5, 0)
-    this.playerBody.linearDamping = 0.9
-    this.world.addBody(this.playerBody)
-
-    // Create a plane
-    var groundShape = new CANNON.Plane()
-    var groundBody = new CANNON.Body({ mass: 0 })
-    groundBody.addShape(groundShape)
-    groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
-    this.world.addBody(groundBody)
+    this.initCameraAndControls()
+    this.initBoxes()
+    this.animate()
   }
 
   private initScene() {
@@ -89,8 +38,6 @@ export class Game {
     let geometry, material, mesh
     var pointerLockScreen
     this.time = Date.now()
-
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
     this.scene = new THREE.Scene()
     this.scene.fog = new THREE.Fog(0x000000, 0, 500)
@@ -115,12 +62,6 @@ export class Game {
     }
     this.scene.add(light)
 
-
-
-    this.controls = new Controls(this.camera, this.playerBody)
-    pointerLockScreen = new PointerLockScreen(this.controls)
-    this.scene.add(this.controls.getObject())
-
     // floor
     geometry = new THREE.PlaneGeometry(300, 300, 50, 50)
     geometry.applyMatrix(new THREE.Matrix4().makeRotationX(- Math.PI / 2))
@@ -139,63 +80,44 @@ export class Game {
     this.renderer.setClearColor(this.scene.fog.color, 1)
 
     document.body.appendChild(this.renderer.domElement)
+  }
 
-    window.addEventListener('resize', onWindowResize.bind(this), false)
+  initCameraAndControls() {
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    this.controls = new Controls(this.camera, this.playerEntity.body)
+    this.scene.add(this.controls.getObject())
+    let pointerLockScreen = new PointerLockScreen(this.controls)
 
-    // Add boxes
-    var halfExtents = new CANNON.Vec3(1, 1, 1)
-    var boxShape = new CANNON.Box(halfExtents)
-    var boxGeometry = new THREE.BoxGeometry(halfExtents.x * 2, halfExtents.y * 2, halfExtents.z * 2)
-    for (var i = 0; i < 7; i++) {
-      var x = (Math.random() - 0.5) * 20
-      var y = 1 + (Math.random() - 0.5) * 1
-      var z = (Math.random() - 0.5) * 20
-      var boxBody = new CANNON.Body({ mass: 5 })
-      boxBody.addShape(boxShape)
-      var boxMesh = new THREE.Mesh(boxGeometry, material)
-      this.world.addBody(boxBody)
-      this.scene.add(boxMesh)
-      boxBody.position.set(x, y, z)
-      boxMesh.position.set(x, y, z)
-      boxMesh.castShadow = true
-      boxMesh.receiveShadow = true
-      this.boxes.push(boxBody)
-      // this.boxMeshes.push(boxMesh)
-      this.meshes.push(boxMesh)
-    }
 
-    function onWindowResize() {
+    window.addEventListener('resize', () => {
       this.camera.aspect = window.innerWidth / window.innerHeight
       this.camera.updateProjectionMatrix()
       this.renderer.setSize(window.innerWidth, window.innerHeight)
-    }
+    }, false)
   }
 
+  initBoxes() {
+    for (var i = 0; i < 7; i++) {
+      let box = new BoxEntity(this.phys.world, this.scene)
+      var x = (Math.random() - 0.5) * 20
+      var y = 1 + (Math.random() - 0.5) * 1
+      var z = (Math.random() - 0.5) * 20
+      box.setPosition(x, y, z)
+      this.entities.push(box)
+    }
+  }
 
   animate() {
     //@ts-ignore
     if (window.stats) window.stats.begin()
 
     if (this.controls.enabled) {
-      this.world.step(DT)
+      this.phys.world.step(DT)
 
-      // // Update ball positions
-      // for (var i = 0; i < this.balls.length; i++) {
-      //   this.ballMeshes[i].position.copy(this.balls[i].position)
-      //   this.ballMeshes[i].quaternion.copy(this.balls[i].quaternion)
-      // }
-
-      // // Update box positions
-      // for (var i = 0; i < this.boxes.length; i++) {
-      //   this.boxMeshes[i].position.copy(this.boxes[i].position)
-      //   this.boxMeshes[i].quaternion.copy(this.boxes[i].quaternion)
-      // }
-
-      // Update box positions
-      for (var i = 0; i < this.meshes.length; i++) {
-        this.meshes[i].position.copy(this.boxes[i].position)
-        this.meshes[i].quaternion.copy(this.boxes[i].quaternion)
+      for (let entity of this.entities) {
+        entity.animate()
       }
+      
     }
 
     this.controls.update(Date.now() - this.time)
--- src/physics-engine.ts
+++ src/physics-engine.ts
@@ -12,18 +12,20 @@ import {
 
 export class PhysicsEngine {
 
-  private world: World
+  public world: World
 
   constructor() {
-    this.world = new CANNON.World()
+    this.world = new World()
+    this._initWorld()
+    this._initMap()
   }
 
-  private initWorld() {
+  private _initWorld() {
     // Setup our world
     this.world.quatNormalizeSkip = 0
     this.world.quatNormalizeFast = false
 
-    let solver = new CANNON.GSSolver()
+    let solver = new GSSolver()
 
     this.world.defaultContactMaterial.contactEquationStiffness = 1e9
     this.world.defaultContactMaterial.contactEquationRelaxation = 4
@@ -32,22 +34,21 @@ export class PhysicsEngine {
     solver.tolerance = 0.1
     var split = true
     if (split)
-      this.world.solver = new CANNON.SplitSolver(solver)
+      this.world.solver = new SplitSolver(solver)
     else
       this.world.solver = solver
 
     this.world.gravity.set(0, -40, 0)
-    this.world.broadphase = new CANNON.NaiveBroadphase()
+    this.world.broadphase = new NaiveBroadphase()
+
+    this.world.addEventListener('solver', console.log)
   }
 
-  private initCannon() {
-    let physicsMaterial, walls = []
-
-    let playerShape = new CANNON.Sphere(PLAYER_RADIUS)
+  private _initMap() {
 
     // Create a slippery material (friction coefficient = 0.0)
-    physicsMaterial = new CANNON.Material("slipperyMaterial")
-    var physicsContactMaterial = new CANNON.ContactMaterial(
+    let physicsMaterial = new Material("slipperyMaterial")
+    let physicsContactMaterial = new ContactMaterial(
       physicsMaterial,
       physicsMaterial,
       {
@@ -58,17 +59,11 @@ export class PhysicsEngine {
     // We must add the contact materials to the world
     this.world.addContactMaterial(physicsContactMaterial)
 
-    // Create a sphere
-    this.playerBody.addShape(playerShape)
-    this.playerBody.position.set(0, 5, 0)
-    this.playerBody.linearDamping = 0.9
-    this.world.addBody(this.playerBody)
-
     // Create a plane
-    var groundShape = new CANNON.Plane()
-    var groundBody = new CANNON.Body({ mass: 0 })
+    var groundShape = new Plane()
+    var groundBody = new Body({ mass: 0 })
     groundBody.addShape(groundShape)
-    groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
+    groundBody.quaternion.setFromAxisAngle(new Vec3(1, 0, 0), -Math.PI / 2)
     this.world.addBody(groundBody)
   }
 }
